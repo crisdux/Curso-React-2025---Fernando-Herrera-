bdc2500fe822f6c73824657a24187fa9abe441bd
crisdux <crisfer.4217@gmail.com>
2025-09-16 17:45:37 -0400

    03-objects-literals.ts

---
+++ 01-practica-ts/src/bases/03-object-literals.ts
@@ -0,0 +1,55 @@
+
+const person: {nombre: string; apellido:string; edad:number;} = {
+    nombre: "Cris",
+    apellido: "Villca",
+    edad: 29,
+}
+
+// console.log(person);
+
+// person.nombre = "Emilio";
+
+// console.log(person)
+
+// es posible cambiar el nombre por que no modificamos el espacio de memoria donde 
+// se almacena el objeto person, solo modificamos el valor de una propiedad de ese objeto
+// por mas que el objeto haya sido declaro con const
+
+//const superHero = person; // no crea un clon de objeto, solo copia la dirección de memoria de person en superHero,
+// en otras palabras ambos objetos apuntan a la misma direccion de memoria
+//superHero.edad = 100
+
+
+// console.log(superHero)
+// console.log(person)
+
+const superHero = {...person}; // usando spread operator hacemos una copia superficial
+superHero.edad = 100; // solo se moficica en superHero
+console.log(superHero, person); // person no se altera
+
+console.log(".".repeat(10))
+
+const animal = {
+    nombre:"boby",
+    items: {
+        color: "cafe",
+        edad:2,
+    }
+}
+
+//const otroAnimal = {...animal}; // pese a hacer una copia con spread no funciona para objetos anidados 
+
+//otroAnimal.items.edad = 10
+// cambiamos el valor de edad tanto en otroAnimal como en animal
+//console.log(animal, otroAnimal)
+
+const otroAnimal = structuredClone(animal);
+otroAnimal.items.edad = 200; // solo cambiamos edad en el objeto anidado otroAnimal
+
+console.log({otroAnimal}, { animal}) // animal mantiene su edad original
+
+
+
+
+
+
--- 01-practica-ts/src/main.ts
+++ 01-practica-ts/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 
 // import "./bases/01-const-let"
-import "./bases/02-template-strings"
+// import "./bases/02-template-strings"
+import  "./bases/03-object-literals"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
